19d7bd34bf1b235ded650bbd25b3d78ce44f548e
cocok <cocok@192.168.86.45>
2020-01-12 14:47:25 -0800

    added a few man pages

--- R/huberRhoPsiWtsEff.R
+++ R/huberRhoPsiWtsEff.R
@@ -1,20 +1,49 @@
-# Huber Rho
+#' Huber Rho
+#' @param x
+#' @param cc
+#' @author Kirk Li  \email{kirkli@@stat.washington.edu} 
+#' @seealso \code{\link{}}
+#' @keywords huber
+#' @examples
+#' @export
 rhoHuber = function(x,cc=1.345)
 {rho = ifelse(abs(x/cc) <1, 0.5*x^2, cc*abs(x)-0.5*cc^2) 
 return(rho)
 }
-# Huber Psi
+#' Huber Psi
+#' @param x
+#' @param cc
+#' @author Kirk Li  \email{kirkli@@stat.washington.edu} 
+#' @seealso \code{\link{}}
+#' @keywords huber
+#' @examples
+#' @export
 psiHuber = function(x,cc=1.345)
 {psi = ifelse(abs(x/cc) < 1, x, cc) 
 psi = ifelse(x/cc <= -1, -cc, psi)
 return(psi)
 }
-# Huber Wts
+#' Huber Wts
+#' @param x
+#' @param cc
+#' @author Kirk Li  \email{kirkli@@stat.washington.edu} 
+#' @seealso \code{\link{}}
+#' @keywords huber
+#' @examples
+#' @export
 wtsHuber = function(x,cc=1.345)
 {wts = ifelse(abs(x/cc) < 1, 1, cc/(sign(x)*x)) 
 return(wts)
 }
-# Efficiency From Constant Huber Psi
+
+#' Efficiency From Constant Huber Psi
+#' @param x
+#' @param cc
+#' @author Kirk Li  \email{kirkli@@stat.washington.edu} 
+#' @seealso \code{\link{}}
+#' @keywords huber
+#' @examples
+#' @export
 effFromConstHuber <- function(cc = 1.345)
 {
   integrand.top <- function(x,cc)
@@ -27,7 +56,14 @@ effFromConstHuber <- function(cc = 1.345)
   varHuber <- nu.top / nu.bottom
   1.0 / varHuber
 }
-# Constant from Efficiency Huber Psi
+#' Constant from Efficiency Huber Psi
+#' @param x
+#' @param cc
+#' @author Kirk Li  \email{kirkli@@stat.washington.edu} 
+#' @seealso \code{\link{}}
+#' @keywords huber
+#' @examples
+#' @export
 constFromEffHuber <- function(eff, interval = c(1e-6, 3))
 {
   obj <- function(cc, e)
