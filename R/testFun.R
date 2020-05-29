161e426506fc0d48fff0366f7ecc62c9f1f13a5b
Kirk Li (AZURE) <klei@microsoft.com>
2020-05-28 18:31:01 -0700

    commit some changes

---
+++ R/testFun.R
@@ -0,0 +1,17 @@
+#' this is a test
+#'
+#' @param test 
+#' @param x 
+#' @param y 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+testFun <- function(test,x,y){
+  print(test)
+  z1 = x+y
+  z2 = x-y
+  return(list(z1,z2))
+}
+
