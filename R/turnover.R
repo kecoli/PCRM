6603978752abbf33b40c0ea2ca4706d0a9393e2a
kirk li <cocokecoli@gmail.com>
2021-11-19 14:42:59 -0800

    changes on R function names

--- R/turnover.R
+++ R/turnover.R
@@ -1,20 +1,30 @@
-#' turnOver
-#'
-#' @param wts 
-#' @param wtsInit 
-#' @param digits 
+#' Turnover
+#' turnover is the function to calculate a times series of n-1 turn-over
+#' values for a times of n portfolio weight vectors, where the turnover
+#' from time i to time i+1 is the sum of the absolute differences 
+#' between the portfolio weights at time i and time i+1
+#' 
+#' @param weights 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-turnOver = function(wts,wtsInit = NULL,digits = NULL)
-{
-if(is.null(wtsInit)) {
-    n = length(wts)
-    wtsInit = rep(1/n,n)}
-to = sum(abs(wts-wtsInit))
-if(is.null(digits)) {list(turnover = to)}
-  else {to = round(to,digits)
-  list(turnover = to)}
+turnover = function(weights){
+  dates = index(weights)
+  weights=coredata(weights)
+  n.asset=ncol(weights)
+  n.dates=nrow(weights)
+  if(n.dates<2){
+    print("Less than 2 balancing dates!")
+    return()
+  }
+  TurnOver=rep(0,n.dates-1)
+  for(i in 1:length(TurnOver)){
+    TurnOver[i]=sum(abs(weights[i+1,]-weights[i,]))
+  }
+  dates=dates[-1]
+  res=zoo(TurnOver,order.by = dates)
+  res
 }
+
---
+++ R/turnoverSimple.R
@@ -0,0 +1,20 @@
+#' turnOverSimple
+#'
+#' @param wts 
+#' @param wtsInit 
+#' @param digits 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+turnoverSimple = function(wts,wtsInit = NULL,digits = NULL)
+{
+if(is.null(wtsInit)) {
+    n = length(wts)
+    wtsInit = rep(1/n,n)}
+to = sum(abs(wts-wtsInit))
+if(is.null(digits)) {list(turnover = to)}
+  else {to = round(to,digits)
+  list(turnover = to)}
+}
